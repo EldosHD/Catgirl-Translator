eaf1e190e34fa829dd23b09f38466b4172ef331a
EldosHD <65720743+EldosHD@users.noreply.github.com>
2021-09-07 23:46:47 +0200

    deleted base

--- Translator.py
+++ Translator.py
@@ -1,13 +1,25 @@
-from flask import Flask, redirect, url_for, render_template
+from os import remove
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def home():
-    return render_template("base.html")
+    if request.method == "POST":
+        inputText = request.form["inputText"]
+        if inputText != None:
+            print("Do sth")
+
+        return render_template("home.html") 
+    else:
+        return render_template("home.html")
+
+@app.route("/next/")
+def next():
+    return render_template("nextWebsite.html")
 
 
 
 if __name__ == "__main__":
-    app.run(port=6969)
+    app.run(debug=True,port=6543)
